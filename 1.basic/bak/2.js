92aa150fa32ce6cc3870aa1676229fafcc4d95be
ningshao007 <ningshaol@outlook.com>
2020-11-02 23:34:59 +0800

    init

--- 1.basic/bak/2.js
+++ 1.basic/bak/2.js
@@ -1,28 +1,14 @@
-
-class Updater {
-    constructor(){
-        this.state={name:'zhufeng',number:0};
-        this.queue = [];
-    }
-    setState(newState){
-        this.queue.push(newState);
-    }
-    flush(){
-        for(let i=0;i<this.queue.length;i++){
-            let update = this.queue[i];
-            if(typeof update === 'function'){
-                this.state = {...this.state,...update(this.state)};
-            }else{
-                this.state = {...this.state,...update};
-            }
-        }
-    }
+class Parent {
+	//静态方法跟类有关系，跟实例没有关系
+	static getParent() {
+		console.log('getParent');
+	}
+	getName() {}
 }
-let updater = new Updater();
-updater.setState({number: 1}); 
-updater.setState((previousState)=>({number: previousState.number+1}));
-updater.setState({number: 2}); 
-updater.setState({number: 3}); 
-
-updater.flush();
-console.log(updater.state);
+class Child extends Parent {
+    // NOTE:如果Child没有下面这个getParent方法的话,则会去用Parent的
+	static getParent() {
+		console.log('getChild');
+	}
+}
+Child.getParent();
--- 1.basic/bak/3.js
+++
@@ -1,9 +0,0 @@
-class Parent{
-    a(){console.log('a');}
-}
-class Child extends Parent{
-    b(){console.log('b');}
-}
-let c = new Child();
-c.b();
-c.a();
